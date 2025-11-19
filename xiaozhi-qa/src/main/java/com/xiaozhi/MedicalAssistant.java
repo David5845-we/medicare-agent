4e0dc7774284ee88a7d4e15cce05da1a3dac345e
david <chendw@jiguang.cn>
2025-11-19 20:05:48 +0800

    test

--- xiaozhi-qa/src/main/java/com/xiaozhi/MedicalAssistant.java
+++ xiaozhi-qa/src/main/java/com/xiaozhi/MedicalAssistant.java
@@ -3,6 +3,7 @@ package com.xiaozhi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 public class MedicalAssistant {
     public static void main(String[] args) {
